605d851cb2a26fdc6be24b5beaa73d513752aa96
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-07-06 12:50:14 +0700

    Update MarkdownRenderer.tsx

--- components/MarkdownRenderer.tsx
+++ components/MarkdownRenderer.tsx
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 
 export default function MarkdownRenderer({ content }: { content: string }) {
   return (
-    <div className="prose prose-sm max-w-none">
+    <div className="prose prose-sm max-w-none break-words">
       <ReactMarkdown>{content}</ReactMarkdown>
     </div>
   );
