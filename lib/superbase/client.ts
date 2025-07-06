b8714b7d96872596312f98800cf6680bc7697240
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-07-06 14:16:28 +0700

    Update client.ts

--- lib/superbase/client.ts
+++ lib/superbase/client.ts
@@ -1,7 +1,8 @@
-import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserClient } from '@supabase/ssr'
 
-export const createClient = () =>
-  createPagesBrowserClient({
-    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-  })
+export function createClient() {
+  return createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )
+}
