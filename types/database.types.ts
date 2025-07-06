c9e4cfebafc3bdf7eb6987dd99350129eba34dff
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-07-06 20:26:46 +0700

    Update database.types.ts

--- types/database.types.ts
+++ types/database.types.ts
@@ -95,6 +95,7 @@ export type Database = {
           id: string
           is_public: boolean
           language: string
+          short_id: string | null
           title: string
           user_id: string
         }
@@ -105,6 +106,7 @@ export type Database = {
           id?: string
           is_public?: boolean
           language: string
+          short_id?: string | null
           title: string
           user_id: string
         }
@@ -115,6 +117,7 @@ export type Database = {
           id?: string
           is_public?: boolean
           language?: string
+          short_id?: string | null
           title?: string
           user_id?: string
         }
