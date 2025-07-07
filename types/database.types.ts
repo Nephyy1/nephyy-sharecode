b8a59657a6ff387eb27fdee2dd1540c5e74f894c
Nephyy <142471741+Nephyy1@users.noreply.github.com>
2025-07-07 20:18:46 +0700

    Update database.types.ts

--- types/database.types.ts
+++ types/database.types.ts
@@ -178,18 +178,21 @@ export type Database = {
           avatar_url: string | null
           full_name: string | null
           id: string
+          role: string
           updated_at: string | null
         }
         Insert: {
           avatar_url?: string | null
           full_name?: string | null
           id: string
+          role?: string
           updated_at?: string | null
         }
         Update: {
           avatar_url?: string | null
           full_name?: string | null
           id?: string
+          role?: string
           updated_at?: string | null
         }
         Relationships: [
